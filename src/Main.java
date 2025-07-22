bb9b49ef905d2f2aae1ae5c2a98eb0a70baf77e1
Nandana Hasheed <nandanahasheed2028@cs.sjcetpalai.ac.in>
2025-07-22 22:41:16 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public void displayDetails() {
     System.out.println("Roll Number: "+rollNumber);
     System.out.println("Grade: "+grade);
 }
- }   
+}   
  public class Main {
     public static void main(String[] args) {
         Student student=new Student();
