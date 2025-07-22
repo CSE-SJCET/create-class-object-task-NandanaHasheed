623a28c4f04736b58e2094a0cd446983531f6059
Nandana Hasheed <nandanahasheed2028@cs.sjcetpalai.ac.in>
2025-07-22 22:36:45 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,13 @@ public void displayDetails() {
     System.out.println("Roll Number: "+rollNumber);
     System.out.println("Grade: "+grade);
 }
-}   
+ }   
  public class Main {
     public static void main(String[] args) {
         Student student=new Student();
         student.name="Nandana";
         student.rollNumber=54;
-        student.grade='A+';
+        student.grade='A';
         student.displayDetails();
-        }
+     }
 }
