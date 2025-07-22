33f860b12932a43a8bc5e407ce07814e9f5f1490
Nandana Hasheed <nandanahasheed2028@cs.sjcetpalai.ac.in>
2025-07-22 22:32:51 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
-     // TODO: Create a class called Student with name, rollNumber, and grade
-
-    // TODO: Create a method to display student details
-public class Main {
-      public static void main(String[] args) {
-        // TODO: Create a Student object and call the method to display details
-    }
+class Student{
+    String name;
+    int rollNumber;
+    char grade;
+public void displayDetails() {
+    System.out.println("Name: "+name);
+    System.out.println("Roll Number: "+rollNumber);
+    System.out.println("Grade: "+grade);
+}
+}   
+ public class Main {
+    public static void main(String[] args) {
+        Student student=new Student();
+        student.name="Nandana";
+        student.rollNumber=54;
+        student.grade='A+';
+        student.displayDetails();
+        }
 }
